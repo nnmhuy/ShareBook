4464d43fbd3d569d075755bc781c285bfe85af19
Minh Huy <nnmhuy@apcs.vn>
2019-08-11 16:45:18 +0700

    add material-ui

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 
+const styles = (theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}))
 
 class App extends React.Component {
   constructor(props) {
@@ -12,9 +22,42 @@ class App extends React.Component {
   }
 
   render() {
+    const { classes } = this.props
     return (
       <div>
         <h1>ShareBook.org.vn</h1>
+        <div>
+          <Button variant="outlined" className={classes.button}>
+            Default
+          </Button>
+          <Button variant="outlined" color="primary" className={classes.button}>
+            Primary
+          </Button>
+          <Button variant="outlined" color="secondary" className={classes.button}>
+            Secondary
+          </Button>
+          <Button variant="outlined" disabled className={classes.button}>
+            Disabled
+          </Button>
+          <Button variant="outlined" href="#outlined-buttons" className={classes.button}>
+            Link
+          </Button>
+          <input
+            accept="image/*"
+            className={classes.input}
+            id="outlined-button-file"
+            multiple
+            type="file"
+          />
+          <label htmlFor="outlined-button-file">
+            <Button variant="outlined" component="span" className={classes.button}>
+                Upload
+            </Button>
+          </label>
+          <Button variant="outlined" color="inherit" className={classes.button}>
+            Inherit
+          </Button>
+        </div>
       </div>
     )
   }
@@ -32,4 +75,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(App));
