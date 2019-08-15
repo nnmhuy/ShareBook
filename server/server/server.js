fe8d01ee7c9702b5d004a315b72d8230f340b6e5
Trinh Huu Duc <yellowthundercat@gmail.com>
2019-08-15 18:04:35 +0700

    env  + rename

--- server/server/server.js
+++ server/server/server.js
@@ -7,6 +7,7 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+require('dotenv').config();
 
 var app = module.exports = loopback();
 
@@ -29,6 +30,9 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
+  if (require.main === module) {
+    // Load environment variable from .env file.
+
     app.start();
+  }
 });
