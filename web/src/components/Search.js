bef5715b843b221d840fbd7b43efc31563a50806
Minh Huy <nnmhuy@apcs.vn>
2019-09-09 12:09:23 +0700

    category book list

---
+++ web/src/components/Search.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import InputBase from '@material-ui/core/InputBase'
+import IconButton from '@material-ui/core/IconButton'
+import { ReactComponent as SearchIcon } from '../static/images/search.svg'
+
+import colors from '../constants/colors'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 270,
+    maxWidth: '80%',
+    borderRadius: 7,
+    boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.16)'
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20
+  }
+}));
+
+export default function Search(props) {
+  const classes = useStyles();
+
+  const { handleChange, handleSearch } = props
+
+  return (
+    <Paper className={classes.root}>
+      <IconButton className={classes.iconButton} aria-label="search" onClick={handleSearch}>
+        <SearchIcon fill={colors.gray} className={classes.icon}/>
+      </IconButton>
+      <InputBase
+        className={classes.input}
+        placeholder='Tìm kiếm'
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </Paper>
+  );
+}
---
+++ web/src/pages/CategoryBookList/demoData.js
@@ -0,0 +1,18 @@
+const bookDemoData = [
+  { bookId: '1', image: require('../../static/images/demo/animal-farm.png')},
+  { bookId: '2', image: require('../../static/images/demo/escape_velocity.png')},
+  { bookId: '3', image: require('../../static/images/demo/in_the_woods.png')},
+  { bookId: '4', image: require('../../static/images/demo/netherland.png')},
+  { bookId: '5', image: require('../../static/images/demo/animal-farm.png') },
+  { bookId: '6', image: require('../../static/images/demo/escape_velocity.png') },
+  { bookId: '7', image: require('../../static/images/demo/in_the_woods.png') },
+  { bookId: '8', image: require('../../static/images/demo/netherland.png') },
+  { bookId: '9', image: require('../../static/images/demo/animal-farm.png') },
+  { bookId: '10', image: require('../../static/images/demo/escape_velocity.png') },
+  { bookId: '11', image: require('../../static/images/demo/in_the_woods.png') },
+  { bookId: '12', image: require('../../static/images/demo/netherland.png') },
+]
+
+export {
+  bookDemoData
+}
