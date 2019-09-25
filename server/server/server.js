df4b599ef46b25694d4f3ce7fe2df0ac39a80a2b
Minh Huy <nnmhuy@apcs.vn>
2019-09-25 18:30:56 +0700

    not work

--- server/server/server.js
+++ server/server/server.js
@@ -73,20 +73,51 @@ for (var s in config) {
   passportConfigurator.configureProvider(s, c);
 }
 
+var https = require('spdy');
+var sslConfig = require('./ssl-config');
+var httpsRedirect = require('./middleware/https-redirect');
+
+app.use(httpsRedirect);
+
 app.start = function() {
   // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+  // return app.listen(function() {
+  //   app.emit('started');
+  //   var baseUrl = app.get('url').replace(/\/$/, '');
+  //   console.log('Web server listening at: %s', baseUrl);
+  //   if (app.get('loopback-component-explorer')) {
+  //     var explorerPath = app.get('loopback-component-explorer').mountPath;
+  //     console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+  //   }
+  // });
+  var httpsOptions = {
+    key: sslConfig.privateKey,
+    cert: sslConfig.certificate,
+  };
+  // start the HTTPS web server
+  return https.createServer(httpsOptions, app).listen(app.get('port'),
+    function() {
+      var baseUrl = 'https://' + app.get('host') + ':' + app.get('port');
+      app.emit('started HTTPS web server', baseUrl);
+      console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+      if (app.get('loopback-component-explorer')) {
+        var explorerPath = app.get('loopback-component-explorer').mountPath;
+        console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      }
+    });
 };
 
-// start the server if `$ node server.js`
-if (require.main === module) {
-  app.start();
-}
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
+// // start the server if `$ node server.js`
+// if (require.main === module) {
+//   app.start();
+// }
---
+++ server/server/ssl-config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+
+exports.privateKey = fs
+  .readFileSync(path.join(__dirname, './private/localhost.key.pem'))
+  .toString();
+exports.certificate = fs
+  .readFileSync(path.join(__dirname, './private/localhost.cert.pem'))
+  .toString();
