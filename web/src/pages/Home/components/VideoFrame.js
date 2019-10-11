a488e63a199d1f34b2e99daa481f8da279bde74f
Minh Huy <nnmhuy@apcs.vn>
2019-10-12 00:12:49 +0700

    allow fullscreen

--- web/src/pages/Home/components/VideoFrame.js
+++ web/src/pages/Home/components/VideoFrame.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CardMedia from '@material-ui/core/CardMedia';
 import { Card } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
@@ -22,10 +21,11 @@ const VideoFrame = () => {
   return (
     <Card className={classes.card}>
       <iframe className={classes.media}
+        title='Introduction Clip'
         src="https://www.youtube.com/embed/nUgFi9daqH8"
         frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen>
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
+      >
       </iframe>
     </Card>
   );
